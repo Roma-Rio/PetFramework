6ffa34ac0e3bf47bd93619a49b217c61aa57fa1f
Roma Rio <9057244137@list.ru>
2020-08-11 18:43:04 +0300

    Badcode

--- src/test/java/by/tut/HomePageTest.java
+++ src/test/java/by/tut/HomePageTest.java
@@ -9,9 +9,12 @@ import org.testng.annotations.Test;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 import java.util.concurrent.TimeUnit;
 
-public class TutbyPage {
+public class HomePageTest {
+    private HomePage homePage;
+    protected ReadProperties properties;
     private WebDriver driver;
     private WebDriverWait wdw;
+
 
     @BeforeTest
     public void start () {
@@ -19,11 +22,13 @@ public class TutbyPage {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
         wdw = new WebDriverWait(driver,10);
+       homePage = new HomePage(driver);
+        properties = new ReadProperties();
     }
 
      @Test
-     public void goToPage () {
-        driver.navigate().to("https://www.tut.by/");
+     public void toHomePage() {
+        driver.get(ReadProperties.getUrlProperty("homepage"));
         wdw.until(titleIs("Белорусский портал TUT.BY. Новости Беларуси и мира"));
      }
 
